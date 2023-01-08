425c1c55ee11f97c90aa7a905167a95ef5aa437e
deavaa <dawitac@gmail.com>
2023-01-08 15:21:50 +0000

    max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return ("None")
+    num = my_list[0]
+    for i in my_list:
+       if i > num:
+            num = i
+    return (num)
