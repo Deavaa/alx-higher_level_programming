cccd3caee460c9c64d6262f8dbb9c9d43a65735c
deavaa <dawitac@gmail.com>
2023-01-24 17:33:11 +0000

    int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    index = 0
-    while True:
-        try:
-            if index < x:
-                print(my_list[index], end='')
-                index += 1
-            else:
-                print()
-                return index
-        except IndexError:
-            print()
-            return index
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        return False
