52df7985b07c8bf1205d835128452306eb98e8ac
deavaa <dawitac@gmail.com>
2023-01-24 16:18:39 +0000

    initialize square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+class Square:
+    """ square."""
+    def __init__(self, size):
+        """Initialize square.
+        """
+        self.__size = size
