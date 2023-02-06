5c777402857a43236b949c34a0319edddd780b74
deavaa <dawitac@gmail.com>
2023-02-06 16:29:07 +0000

    class that inherited

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def is_kind_of_class(obj, a_class):
+    '''function: is_kind_of_class
+    obj: an object
+    a_class: a class
+    Returns: Bool
+    '''
+    return isinstance(obj, a_class)
